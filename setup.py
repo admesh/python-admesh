c350318f4f2673023e1b08c156a5e7a1247322fc
Miro Hrončok <miro@hroncok.cz>
2014-08-29 22:16:15 +0200

    Add some more classifiers

--- setup.py
+++ setup.py
@@ -23,7 +23,13 @@ setup(
                  'Operating System :: POSIX :: Linux',
                  'Operating System :: MacOS :: MacOS X',
                  'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: Implementation :: PyPy',
                  'Topic :: Multimedia :: Graphics :: 3D Modeling',
                 ],
 )
